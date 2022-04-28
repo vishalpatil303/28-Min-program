db6970d3641389099ecdd52cc4ec9b4756a1a284
vishalpatil303 <104244091+vishalpatil303@users.noreply.github.com>
2022-04-28 10:31:09 +0530

    Update BinarySortImp.java

--- src/main/java/com/mycompany/BinarySortImp.java
+++ src/main/java/com/mycompany/BinarySortImp.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qulifer("BinarySortImp")
 public class BinarySortImp {
 
 	@Autowired
