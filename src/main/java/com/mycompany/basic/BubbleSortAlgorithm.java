9acc823e1a5cf64ca3bf25dd9c68f48495105049
Vishal <Vishal@VISHAL>
2022-04-28 21:46:19 +0530

    # WARNING: head commit changed in the meantime
    commit all

--- src/main/java/com/mycompany/basic/BubbleSortAlgorithm.java
+++ src/main/java/com/mycompany/basic/BubbleSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.mycompany.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -10,10 +13,24 @@ import org.springframework.stereotype.Component;
 public class BubbleSortAlgorithm implements SortAlgotrithm
 {
 
+	public BubbleSortAlgorithm()
+	{
+		System.out.println("Bubble Sort Algorithm Constructor");
+	}
 	@Override
 	public int[] sort(int[] numbers) {
 		System.out.println("Bubble Sort Algorithm");
 		return numbers;
+	}
+	@PostConstruct
+	public void postConstruct()
+	{
+		System.out.println("Bubble Sort Algorithm post construct method");
+	}
+	@PreDestroy
+	public void preDestory()
+	{
+		System.out.println("Bubble Sort Algorithm pre Distory construct method");
 	}
 
 }
--- src/main/java/com/mycompany/basic/QuickSortAlgorithm.java
+++ src/main/java/com/mycompany/basic/QuickSortAlgorithm.java
@@ -1,5 +1,8 @@
 package com.mycompany.basic;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
@@ -10,10 +13,24 @@ import org.springframework.stereotype.Component;
 @Qualifier("Quick")
 public class QuickSortAlgorithm implements SortAlgotrithm{
 
+	public QuickSortAlgorithm()
+	{
+		System.out.println("Quick Sort Algorithm Constructor");
+	}
 	@Override
 	public int[] sort(int[] numbers) {
 		System.out.println("Quick Sort Algorithm");
 		return numbers;
 	}
 
+	@PostConstruct
+	public void postConstruct()
+	{
+		System.out.println("Quick Sort Algorithm post construct method");
+	}
+	@PreDestroy
+	public void preDestory()
+	{
+		System.out.println("Quick Sort Algorithm pre Distory construct method");
+	}
 }
